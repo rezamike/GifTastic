1c8c5ae6f5fdb79741f266554205e504c7e7ee83
rezamike <michael.sanaiha@gmail.com>
2018-09-02 02:40:17 -0700

    building that wireframe

---
+++ assets/javascript/main.js
@@ -0,0 +1,26 @@
+$("document").ready(function () {
+
+    // build function for on.click
+    $("button").on("click", function () {
+
+        // build var for each of the buttons
+        var animal = $(this).attr()
+
+        // create ajax get
+
+
+        // global var for search terms to dom
+
+
+        // var for image property
+
+
+        // creating image tag
+
+
+        // setting attributs to the dom
+
+
+        // prepending everything
+    })
+})
