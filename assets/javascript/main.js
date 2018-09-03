7cf450660f58961d39817fe6fba8ac29bcd3cfb5
rezamike <michael.sanaiha@gmail.com>
2018-09-02 19:45:32 -0700

    i think i'm going places

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,24 +9,58 @@ $("document").ready(function () {
         "steak",
         "burrito",
     ];
+    var apiKey = "Slxk7tck3X7EpB9VouJWIMx6gh04Vq3C";
 
     buildBtn();
 
+    $(".foods").on("click", function foodInfo() {
+            var food = $(this).attr("data-food");
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=" + apiKey + "&limit=10";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                var foodDiv = $("<div class='food'>");
+
+                var rating = response.rating;
+                var pRating = $("<p>").text("Rating: " + rating);
+                foodDiv.append(pRating);
+
+                var imgUrl = response.images.original.url;
+                var image = $("<img>").attr("src", imgUrl)
+                foodDiv.append(image);
+                console.log(imgUrl)
+
+                $(".showRoom").append(foodDiv);
+
+            })
+    })
+
+
     // global on-click/var for search terms to dom
     $("#addFood").on("click", function (event) {
+        event.preventDefault();
         var searchBtn = $("#food-input").val();
-        if (searchBtn != food) {
-           food.push(searchBtn); 
+        if (searchBtn === "") {
+            alert("Empty!");
+        }
+        else {
+            food.push(searchBtn);
+        }
+        if (!(searchBtn in food)) {
+            alert("You already typed that one!");
         }
         buildBtn();
-        event.preventDefault();
     })
 
 
     // for loop to run through food array
     function buildBtn() {
+        $("#searchButtons").empty();
         for (var i = 0; i < food.length; i++) {
             var foodBtn = $("<button>").attr("data-food", food[i]);
+            foodBtn.attr("class", "foods")
             $(foodBtn).text(food[i]);
 
             // create the buttons
@@ -35,13 +69,8 @@ $("document").ready(function () {
         }
     }
 
-
-
-
-
-
     // build function for on.click
-    // $("button" + i).on("click", function () {
+    $("button").on("click", function () {
 
         // build var for each of the buttons
 
@@ -59,5 +88,5 @@ $("document").ready(function () {
 
 
         // prepending everything
-    // })
+    })
 })
