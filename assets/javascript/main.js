1ba6c46af7c4dd15aa176fee0bc2110d266732cf
rezamike <michael.sanaiha@gmail.com>
2018-09-02 18:16:15 -0700

    stuck

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,34 +7,44 @@ $("document").ready(function () {
         "pizza",
         "ramen",
         "steak",
-        "burrito"
+        "burrito",
     ];
+
+    buildBtn();
 
     // global on-click/var for search terms to dom
     $("#addFood").on("click", function (event) {
         var searchBtn = $("#food-input").val();
-        console.log(searchBtn);
+        if (searchBtn != food) {
+           food.push(searchBtn); 
+        }
+        buildBtn();
         event.preventDefault();
-        food.push(searchBtn);
     })
 
 
     // for loop to run through food array
-    for (var i = 0; i < food.length; i++) {
-        var foodBtn = $("<button>").attr("data-food", food[i]);
-        $(foodBtn).text(food[i]);
+    function buildBtn() {
+        for (var i = 0; i < food.length; i++) {
+            var foodBtn = $("<button>").attr("data-food", food[i]);
+            $(foodBtn).text(food[i]);
 
-        // create the buttons
-        $("#searchButtons").append(foodBtn);
+            // create the buttons
+            $("#searchButtons").append(foodBtn);
 
+        }
     }
 
 
+
+
+
+
     // build function for on.click
-    $("button" + i).on("click", function () {
+    // $("button" + i).on("click", function () {
 
         // build var for each of the buttons
-        
+
 
         // create ajax get
 
@@ -49,5 +59,5 @@ $("document").ready(function () {
 
 
         // prepending everything
-    })
+    // })
 })
